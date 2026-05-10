4173c277f5e1ddfb41b8b1d81dee0050723a928b
MehboobAli-Portfolio <mehboob56ali78@gmail.com>
2026-05-10 19:24:54 +0500

    feat: PPP-based affordability engine, remove dead COLI API, Redis-safe caching, schema hardening

--- finora-backend/salary_reality/admin.py
+++ finora-backend/salary_reality/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import SalaryProfile, SalarySnapshot, GlobalBenchmark, ExchangeRate
-
+from .models import SalaryProfile, SalarySnapshot
 
 @admin.register(SalaryProfile)
 class SalaryProfileAdmin(admin.ModelAdmin):
@@ -8,19 +7,7 @@ class SalaryProfileAdmin(admin.ModelAdmin):
     list_filter = ('country', 'industry')
     search_fields = ('user__email', 'job_title')
 
-
 @admin.register(SalarySnapshot)
 class SalarySnapshotAdmin(admin.ModelAdmin):
     list_display = ('user', 'salary_usd', 'global_pctile', 'country_pctile', 'recorded_at')
     list_filter = ('recorded_at',)
-
-
-@admin.register(GlobalBenchmark)
-class GlobalBenchmarkAdmin(admin.ModelAdmin):
-    list_display = ('country_name', 'industry', 'avg_salary_usd', 'median_salary_usd', 'data_year')
-    list_filter = ('country', 'industry', 'data_year')
-
-
-@admin.register(ExchangeRate)
-class ExchangeRateAdmin(admin.ModelAdmin):
-    list_display = ('base_currency', 'target_currency', 'rate', 'fetched_at')
